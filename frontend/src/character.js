078436eec389026fb6a8a7e72b067286ccacfad7
chica25 <patricia@patriciaco.com>
2020-12-06 22:32:38 -0500

    Render character class

--- frontend/src/character.js
+++ frontend/src/character.js
@@ -1,24 +1,35 @@
 class Character {
     static container = document.querySelector('.character-col')
-    constructor() {
-        this.getImages()
+    constructor(character = null) {
+        if (character) {
+            this.render(character)
+        } else {
+            this.getImages()
+        }
+
         // this.attachEventListener()
     }
     getImages() {
         api.fetchImages()
     }
     static renderImages(data) {
-        data.forEach(character => render(character))
+        data.forEach(character => new Character(character))
     }
     render(character){
         const card = document.createElement("div")
         card.className = "card"
         this.card = card
-        this.renderInnerHTML()
+        this.card.innerHTML = `
+        ${character.name}
+        <img src="${character.image_url}">
+        <button class="button">Like Me!</button>
+        `
+        buttonEventListener()
+        // Here is where you want to code the button event listener 
+        // Hint: you have to code it with card passed in.
+        this.constructor.container.append(card)
+
     }
-        renderInnerHTML() {
-            this.card.innerHTML = `
-            <img src="${thi}
-            `
-        }
+    buttonEventListner
+
 }
