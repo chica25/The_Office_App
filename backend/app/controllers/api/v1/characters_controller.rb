d959b8c6c7016f4fda9c3afc1083aa3d8daa5b03
chica25 <patricia@patriciaco.com>
2020-12-12 00:07:16 -0500

    Edit controller actions for all  models

--- backend/app/controllers/api/v1/characters_controller.rb
+++ backend/app/controllers/api/v1/characters_controller.rb
@@ -1,5 +1,5 @@
 class Api::V1::CharactersController < ApplicationController
-  before_action :set_character, only: [:show, :update, :destroy]
+  before_action :set_character, only: [:update]
 
   # GET /characters
   def index
@@ -12,26 +12,16 @@ class Api::V1::CharactersController < ApplicationController
 
   # PATCH/PUT /characters/1
   def update
-    byebug
     @character = Api::V1::Toy.find_by_id(params[:id])
     @character.update(likes: @character.likes + 1)
     render json: @character, except: [:created_at, :updated_at]
   
   end
 
-  # DELETE /characters/1
-  # def destroy
-  #   @character.destroy
-  # end
-
+ 
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_character
       @character = Character.find(params[:id])
-    end
-
-    # Only allow a trusted parameter "white list" through.
-    def character_params
-      params.require(:character).permit(:name, :image_url)
     end
 end
