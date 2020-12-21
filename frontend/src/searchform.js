bd5b26ae601d42acba1c278e5f15c3ba8a5c48f7
chica25 <patricia@patriciaco.com>
2020-12-20 23:41:25 -0500

    Create search functions and event listener for the searchForm class

--- frontend/src/searchform.js
+++ frontend/src/searchform.js
@@ -6,11 +6,20 @@ class SearchForm {
     const form = document.getElementById("search-form")
     console.log(form)
     form.innerHTML = `
-        <form action="GET">
+        <form action="" id="search-form" method="get">
             <label for="search"></label><br>
             <input type="text" name="search" placeholder="Search by Name"></input>
             <input type="submit" value="Submit"></input>    
         </form>
         `
         }
+        formEventListener = (request) => {
+            const form = document.getElementById("search-form")
+            formEventListener("submit", (e) => this.searchEvent(e));
+        }
+        searchEvent = (e) => {
+            e.preventDefault();
+            const formName = {name: event.target.search.value}
+        }
     }
+  
