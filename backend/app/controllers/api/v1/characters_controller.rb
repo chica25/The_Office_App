1bf8b64df1ffc918fc7191e2f79e4e9fb53dc476
chica25 <patricia@patriciaco.com>
2020-12-25 23:16:57 -0500

    last commit

--- backend/app/controllers/api/v1/characters_controller.rb
+++ backend/app/controllers/api/v1/characters_controller.rb
@@ -1,17 +1,14 @@
 class Api::V1::CharactersController < ApplicationController
 
-  # GET /characters
   def index
-    @characters = Api::V1::Character.all
-     render json: @characters, except: [:created_at, :updated_at]
+    characters = Api::V1::Character.all
+     render json: characters, except: [:created_at, :updated_at]
   end
 
-
-  # PATCH - /characters/:id/quotes
   def update
-    @character = Api::V1::Character.find_by_id(params[:id])
-    @character.update(likes: @character.likes + 1)
-    render json: @character, except: [:created_at, :updated_at]
+    character = Api::V1::Character.find_by_id(params[:id])
+    character.update(likes: character.likes + 1)
+    render json: character, except: [:created_at, :updated_at]
   
   end
 
--- backend/app/controllers/api/v1/quotes_controller.rb
+++ backend/app/controllers/api/v1/quotes_controller.rb
@@ -1,9 +1,9 @@
 class Api::V1::QuotesController < ApplicationController
 
   def index
-    @quotes = Api::V1::Quote.where('character_id = ?', params[:character_id])
+    quotes = Api::V1::Quote.where('character_id = ?', params[:character_id])
 
-    render json: @quotes, include: [:character], except: [:created_at, :updated_at]
+    render json: quotes, include: [:character], except: [:created_at, :updated_at]
   end
 end
   
